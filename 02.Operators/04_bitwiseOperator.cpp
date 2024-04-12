3fd8a850cbd1ec4d6cb8c1a0537bb0db6ae2e0fb
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-12 14:01:14 +0530

    bitwise

---
+++ 02.Operators/04_bitwiseOperator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // bool a=true;
+    // bool b=true;
+
+    // cout<< (a&b) << endl;
+    // cout<< (a|b) << endl;
+    // cout<< (a^b) << endl;
+
+    bool a=true;
+    bool b=false;
+
+    // cout<< (a&b) << endl;
+    // cout<< (a|b) << endl;
+    // cout<< (a^b) << endl;
+    // cout<< (~a) << endl;
+    // cout<< (~b) << endl;
+
+    cout<< (2&3) << endl;
+    cout<< (5&10) << endl;
+}
+   

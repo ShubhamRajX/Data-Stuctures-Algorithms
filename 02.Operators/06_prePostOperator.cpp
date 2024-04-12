432f6f8398babee1eaacfd534e046e271211d596
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-12 14:26:58 +0530

    incermentDecrement

--- 02.Operators/06_prePostOperator.cpp
+++ 02.Operators/06_prePostOperator.cpp
@@ -5,21 +5,26 @@ int main()
 {
     int a = 5;
 
-// cout << a << endl;
+    // cout << a << endl;
 
-//     cout << ++a << endl;
+    // cout << ++a << endl;
 
-//     cout << --a << endl;
+    // cout << --a << endl;
 
-//     cout << a++ << endl;
-//     cout << a << endl;
+    // cout << a++ << endl;
+    // cout << a << endl;
 
-//     cout << a-- << endl;
-//     cout << a << endl;
+    // cout << a-- << endl;
+    // cout << a << endl;
 
-//  a++;
- //  cout << a << endl;
+    // a++;
+    // cout << a << endl;
 
-     a--;
-     cout << a << endl;
+    // a--;
+    // cout << a << endl;
+
+
+    // int c = (a++) + 1;
+    int c = ++a + 1;
+    cout << c <<endl;
 }

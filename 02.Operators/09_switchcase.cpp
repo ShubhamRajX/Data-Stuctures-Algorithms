1c0f52cb9f61465a834420b120fb7062b544f1b3
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-12 22:01:07 +0530

    switchCase

---
+++ 02.Operators/09_switchcase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    switch(n)
+    {
+        case 1 : cout <<" Shubham";
+        break;
+        case 2 : cout <<"Raj";
+        break;
+        case 3 : cout <<"Gupta";
+        break;
+        default : cout <<"Anisha Das";
+    }
+}

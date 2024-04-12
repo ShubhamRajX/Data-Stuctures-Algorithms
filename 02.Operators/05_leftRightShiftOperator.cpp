dfd5faf4aca0a78426e0aa764b4385b9f210b1d8
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-12 14:01:39 +0530

    shiftOperator

---
+++ 02.Operators/05_leftRightShiftOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 5;
+    // a = a << 1;
+
+    //  int a = 4;
+    // a = a << 2;
+
+    // int a = 12;
+    // a = a >> 1;
+
+    int a = -27;
+    // a =  a >> 1;
+    a =  a << 1;
+
+    cout << a <<endl;
+}

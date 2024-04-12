39b771e08be2ba88b726fa061d5a455b2f92817c
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-12 21:15:29 +0530

    keywords

---
+++ 02.Operators/07_breakContinue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<"Shubham" << endl;
+    //     break;     //it break the loop
+    // }
+    // cout<<"Raj" <<endl;
+
+
+
+    for(int i=0;i<n;i++)
+    {
+
+        continue;          //it skip all the term
+        cout<<"Shubham" << endl;
+    }
+    cout<<"Raj" <<endl;
+}

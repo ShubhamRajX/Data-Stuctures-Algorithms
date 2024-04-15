465d7cc244096da1b2f6a6fdb93fc5c7a30882fd
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-16 00:09:23 +0530

    reverseDigit

---
+++ 06.function/13_reverseIntegers.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int reverse(int n)
+{
+    int ans =0, rem=0;
+    bool isNeg=false;
+
+    if(x <= INT_MIN)
+    {
+        return 0;
+    }
+    if(x<0)
+    {
+        isNeg=true;
+        x=-x;
+    }
+    while(x>0)
+    {
+        if (ans> INT_MAX / 10)
+        {
+            return 0;
+        }
+        int digit = x%10;
+        ana=ans*10+digit;
+        x=x/10;
+    }
+    return isNeg? -ans: ans;
+}
+}

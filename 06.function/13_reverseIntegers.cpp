9430ff827d5e3b23a95c988d6f6d2ca2a15e00d4
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-16 23:11:19 +0530

    leetcode

--- 06.function/13_reverseIntegers.cpp
+++ 06.function/13_reverseIntegers.cpp
@@ -1,31 +1,31 @@
-//leetcode
-class solution
-{
-    public:
-int reverse(int n)
-{
-    int ans =0, rem=0;
-    bool isNeg=false;
+// //leetcode
+// class solution
+// {
+//     public:
+// int reverse(int n)
+// {
+//     int ans =0, rem=0;
+//     bool isNeg=false;
 
-    if(x <= INT_MIN)
-    {
-        return 0;
-    }
-    if(x<0)
-    {
-        isNeg=true;
-        x=-x;
-    }
-    while(x>0)
-    {
-        if (ans> INT_MAX / 10)
-        {
-            return 0;
-        }
-        int digit = x%10;
-        ana=ans*10+digit;
-        x=x/10;
-    }
-    return isNeg? -ans: ans;
-}
-}
+//     if(x <= INT_MIN)
+//     {
+//         return 0;
+//     }
+//     if(x<0)
+//     {
+//         isNeg=true;
+//         x=-x;
+//     }
+//     while(x>0)
+//     {
+//         if (ans> INT_MAX / 10)
+//         {
+//             return 0;
+//         }
+//         int digit = x%10;
+//         ana=ans*10+digit;
+//         x=x/10;
+//     }
+//     return isNeg? -ans: ans;
+// }
+// }
--- 06.function/15_convertTemp.cpp
+++ 06.function/15_convertTemp.cpp
@@ -1,15 +1,15 @@
-//leetcode
-class solution
-{
-    public:
-    vector<double> convertTemp(double celsius)
-    {
-        double k = celsius + 273.15;
-        double f = celsius * 1.00 + 32.00;
+// //leetcode
+// class solution
+// {
+//     public:
+//     vector<double> convertTemp(double celsius)
+//     {
+//         double k = celsius + 273.15;
+//         double f = celsius * 1.00 + 32.00;
 
-        vector<double> v;
-        v.push_back(k);
-        v.push_back(f);
-        return v;
-    }
-};
+//         vector<double> v;
+//         v.push_back(k);
+//         v.push_back(f);
+//         return v;
+//     }
+// };

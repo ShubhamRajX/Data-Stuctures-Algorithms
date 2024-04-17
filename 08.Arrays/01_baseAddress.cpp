d26c907d6499bc1783763c84742c4be0205f4e3e
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-04-18 01:26:30 +0530

    address

---
+++ 08.Arrays/01_baseAddress.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Array declare
+    // int arr[7];
+
+    // base address
+    // cout<< arr << endl;
+    // cout<< &arr << endl;
+
+    // cout << "Array created successfully" << endl; 
+
+    // int arr[]={2,4,6,8,10};
+    // int b[5]={2,4,6,8,10};
+    int arr[10]={2,4,6,8,10};
+
+    //error due to less sizs allocte
+    // int arr[4]={2,4,6,8,10};
+
+    cout << "Array initialised successfully" << endl;
+}
---
+++ 08.Arrays/02_address.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int arr[]={1,3,5,7,9};
+
+    // // cout<< arr[2] <<endl;
+
+    // //printing all values
+    // for(int index=0;index<5;index++)
+    // {
+    //     cout<<arr[index]<<" ";
+    // }
+
+
+    // int arr[10];
+    // int arr[10]={1,2,};
+    // int arr[10]={0};
+    int arr[10]={1};
+
+    for(int i=0;i<10;i++)
+    {
+        cout<<arr[i]<<"  ";
+    }
+}

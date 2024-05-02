248ce1533f8e5befc86c307d4ff36dc1f9e56482
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-02 23:49:06 +0530

    basic

---
+++ 09.2D Array/2D_Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int arr[3][3];
+
+    // int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+    // int arr[2][3] = {{1,2,3},{4,5,6}};
+
+    int arr[3][2] = {
+        {1,2},
+        {4,5},
+        {7,8}};
+
+    // cout<<arr[1][2];
+    cout<<arr[2][1];
+}

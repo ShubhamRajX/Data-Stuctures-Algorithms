bc23d936258388a27444f64c97aefb6f25a4eb81
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-03 19:31:56 +0530

    rowSumUsingFunction

---
+++ 09.2D Array/05_rowSum.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+void sumFunction(int arr[3][3], int row, int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        int sum = 0;
+        for(int j=0;j<col;j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout<<sum<<endl;
+    }
+    
+
+}
+
+int main()
+{
+    int arr[3][3];
+
+    int row = 3;
+    int col = 3;
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    sumFunction(arr,  row, col);
+    
+}
+
+    

7383734ce8ed1389eb16019c2901b35a2447e5af
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-05 23:23:08 +0530

    minimunNumber

---
+++ 09.2D Array/09_minimumNumber.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int main()
+{
+    int arr[3][3];
+
+    int row = 3;
+    int col = 3;
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    
+    int min = INT_MAX;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            if(arr[i][j] < min)
+            {
+                min = arr[i][j];
+            }
+        }
+    }
+    cout <<min;
+}

ca03c01fc80f6bd68fa19b13bc395ad713bcbdb8
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-05 23:09:47 +0530

    linearSearch

--- 09.2D Array/07_linearSearch.cpp
+++ 09.2D Array/07_linearSearch.cpp
@@ -1,47 +1,41 @@
 #include<iostream>
 using namespace std;
 
-bool findKey(int arr[][3],int row,int col,int key)
+bool findKey(int arr[][3],int col,int row,int key)
 {
-    for(int i=0;i,row;i++)
+    for(int i=0;i<row;i++)
     {
         for(int j=0;j<col;j++)
         {
             if(arr[i][j] == key)
-            {
-                return true;
-            }
-            else{
-                return false;
-            }
+            return true;
         }
     }
-    
+    return false;
 }
 
 int main()
 {
-    int arr[3][3] = {
-        {5,6,8},
-        {7,2,4},
-        {1,6,9}};
+    int arr[3][3];
 
     int row = 3;
     int col = 3;
 
-    int key = 41;
+    int key = 80;
 
-    // int or bool both can use
-    // int find = findKey(arr,3,3,key);
-
-    //here in if cond. you give "find" or "findKey(arr,3,3,key)"
-    if (findKey(arr,3,3,key))
+    for(int i=0;i<row;i++)
     {
-        cout<<"True";
+        for(int j=0;j<col;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    if(findKey(arr,3,3,key))
+    {
+        cout<<"True"<<endl;
     }
     else{
-        cout<<"False";
+        cout<<"False"<<endl;
     }
-    // cout << find;
-    return 0; 
 }

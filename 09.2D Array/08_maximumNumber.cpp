81c7d16c7329c033a6a1b2dff5080ee3cc446e10
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-05 23:16:23 +0530

    maximumNumber

--- 09.2D Array/08_maximumNumber.cpp
+++ 09.2D Array/08_maximumNumber.cpp
@@ -2,13 +2,28 @@
 #include<limits.h>
 using namespace std;
 
+int getMax(int arr[][3],int row,int col)
+{
+    int max = INT_MIN;
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            if(arr[i][j] > max)
+            {
+                max = arr[i][j];
+            }
+        }
+    }
+    return max;
+}
+
 int main()
 {
     int arr[3][3];
 
     int row = 3;
     int col = 3;
-    int max = INT_MIN;
 
     for(int i=0;i<row;i++)
     {
@@ -17,17 +32,5 @@ int main()
             cin>>arr[i][j];
         }
     }
-    //printing value
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<col;j++)
-        {
-            if(arr[i][j] > INT_MIN)
-            
-            {
-                max = arr[i][j];
-            }
-        }
-        cout<<max;
-    }
+    cout<<getMax(arr,row,col);
 }

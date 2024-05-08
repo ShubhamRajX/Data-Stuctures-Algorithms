aeb3a1606512b17fc0e0ff0d807a6b923deeec93
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-09 01:37:45 +0530

    convertTemperature

--- 06.function/15_convertTemp.cpp
+++ 06.function/15_convertTemp.cpp
@@ -1,15 +1,57 @@
-// //leetcode
-// class solution
-// {
-//     public:
-//     vector<double> convertTemp(double celsius)
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number:- ";
+    cin>>n;
+
+    // double k;
+    // double f;
+
+    double k = n + 273.15;
+    double f = n * 1.80 + 32.00;
+
+    cout<<k<<endl;
+    cout<<f<<endl;
+}
+
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// double convertTemp(vector<double>arr)
 //     {
+//         double celsius;
+
 //         double k = celsius + 273.15;
 //         double f = celsius * 1.00 + 32.00;
 
-//         vector<double> v;
-//         v.push_back(k);
-//         v.push_back(f);
-//         return v;
+//         vector<double> arr;
+//         arr.push_back(k);
+//         arr.push_back(f);
+
+//         return arr;
 //     }
-// };
+
+// int main()
+// {
+//     double n;
+//     cin>>n;
+//     cout<<"Enter nmber:-";
+    
+//     vector<double>arr(n);
+
+//     for(int i=0;i<n;i)
+//     {
+//         cin>>arr[i];
+//     }
+    
+// double result = convertTemp(arr);
+
+// cout<<result;
+// }
+
+

e083a76f3c67b5bc9623ab36912430570d0c93a2
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-09 16:29:11 +0530

    setKthBit

--- 06.function/14_setBit.cpp
+++ 06.function/14_setBit.cpp
@@ -1,12 +1,26 @@
-// //gfg
-// class solution
-// {
-//     public:
-//     int setKthBit(int N ,int K)
-//     {
-//         //write your code here
-//         int mask -1 <<K;
-//         int ans= N/mask;
-//         return ans;
-//     }
-// };
+#include<iostream>
+using namespace std;
+
+int setKthBit(int N ,int K)
+    {
+        //write your code here
+        int mask = 1 << K;
+        int ans = N | mask;
+        return ans;
+
+
+        // return N | (1<<K);
+    }
+
+int main()
+{
+    int N;
+    cin>>N;
+
+    int K;
+    cin>>K;
+
+    int result = setKthBit(N,K);
+
+    cout <<result;
+}

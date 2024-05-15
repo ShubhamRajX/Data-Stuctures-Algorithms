ca6f36c6e22f11458b1404dae8ae2f8d39ca0bef
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-15 12:16:19 +0530

    usingFunction

---
+++ 10.Searching/05_first_lastOccurenceFunction.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+int main()
+{
+   int n;
+   cin>>n;
+   vector<int>arr(n);
+
+   for(int i=0;i<n;i++)
+   {
+    cin>>arr[i];
+   }
+   int a;
+   cout <<"Enter target element: ";
+   cin>>a;
+
+   int target = a;
+
+    auto ans1 = lower_bound (arr.begin(), arr.end(), target);
+    auto ans2 = upper_bound (arr.begin(), arr.end(), target);
+   
+
+    cout<<"First Occurence is: " <<ans1-arr.begin() <<" index"<<endl;
+    cout<<"Last Occurence is: " <<ans2-arr.begin() <<" index"<<endl;
+
+}

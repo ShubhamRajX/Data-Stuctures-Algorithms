231af3143e5cc141cef8d0824df3a5d5eddcefa3
ShubhamRajX <shubhamraj.1517@gmail.com>
2024-05-15 11:17:50 +0530

    findFirstOccurence

---
+++ 10.Searching/03_firstOccurence.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int firstOccurence(vector<int>arr,int target)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+
+    int mid = start + (end-start)/2;
+    int ans = -1; 
+
+    while(start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            //store ans then search in left side
+            ans = mid;
+            end = mid -1;
+        }
+        else if(target > arr[mid])
+        {
+            //search in right
+            start = mid + 1;
+        }
+        else if (target < arr[mid])
+        {
+            //search in left
+            start = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int>arr{1,3,4,4,4,4,4,6,7,9};
+    int target = 4;
+
+    int result = firstOccurence(arr, target);
+
+    cout<<"ans is: "<<result<<endl;
+}
